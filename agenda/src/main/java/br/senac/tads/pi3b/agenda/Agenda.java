21179d9a0c49a8fe8371684511d86f3d3998e752
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-02-27 22:32:00 -0300

    Entrada de dados

--- agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
@@ -10,10 +10,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -33,7 +39,7 @@ public class Agenda {
     public List<Pessoa> listar() throws ClassNotFoundException, SQLException {
 
         List<Pessoa> lista = new ArrayList<Pessoa>();
-        
+
         try (Connection conn = obterConexao();
                 PreparedStatement stmt = conn.prepareStatement(
                         "SELECT id, nome, dtnascimento FROM PESSOA");
@@ -54,14 +60,13 @@ public class Agenda {
         return lista;
     }
 
-    public void incluir() throws ClassNotFoundException, SQLException {
+    public void incluir(Pessoa p, String email, String telefone) throws ClassNotFoundException, SQLException {
 
         try (Connection conn = obterConexao();
                 PreparedStatement stmt = conn.prepareStatement(
                         "INSERT INTO PESSOA (nome, dtnascimento) VALUES (?,?)")) {
-            stmt.setString(1, "MARIA DE SOUZA");
-            GregorianCalendar cal = new GregorianCalendar(1992, 10, 5); // 5 de novembro de 1992  
-            stmt.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
+            stmt.setString(1, p.getNome());
+            stmt.setDate(2, new java.sql.Date(p.getDtNascimento().getTime()));
 
             int status = stmt.executeUpdate();
             System.out.println("Status: " + status);
@@ -72,7 +77,29 @@ public class Agenda {
         Agenda agenda = new Agenda();
 
         try {
-            //agenda.incluir();
+            try (Scanner in = new Scanner(System.in)) {
+                System.out.println("Digite o nome:");
+                String nome = in.nextLine();
+                System.out.println("Digite a data de nascimento no formato dd/MM/yyyy:");
+                String strData = in.nextLine();
+                System.out.println("Digite o email:");
+                String email = in.nextLine();
+                System.out.println("Digite o telefone no formato (99) 99999-9999:");
+                String telefone = in.nextLine();
+                
+                DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+                Date dtNascimento = null;
+                try {
+                    dtNascimento = formatador.parse(strData);
+                } catch (ParseException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                Pessoa p = new Pessoa();
+                p.setNome(nome);
+                p.setDtNascimento(dtNascimento);
+                agenda.incluir(p, email, telefone);
+            }
+            
             List<Pessoa> lista = agenda.listar();
             for (Pessoa p : lista) {
                 System.out.println(p.getId() + ", " + p.getNome() + ", " + p.getDtNascimento());
